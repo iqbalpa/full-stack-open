ed15b6139580ebda477f0e6a4f5dcaeb401b78e2
iqbalpa <iqbalpahlevi16@gmail.com>
2023-06-23 17:05:38 +0700

    part 3.9: done

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const getAll = () => {
-	return axios.get("http://localhost:3001/persons");
+	return axios.get("http://localhost:3001/api/persons");
 };
 const addContact = (person) => {
-	return axios.post("http://localhost:3001/persons", person);
+	return axios.post("http://localhost:3001/api/persons", person);
 };
 const deleteContact = (id) => {
-	return axios.delete(`http://localhost:3001/persons/${id}`);
+	return axios.delete(`http://localhost:3001/api/persons/${id}`);
 };
 const updateContact = (id, person) => {
-	return axios.put(`http://localhost:3001/persons/${id}`, person);
+	return axios.put(`http://localhost:3001/api/persons/${id}`, person);
 };
 
 export default { getAll, addContact, deleteContact, updateContact };
