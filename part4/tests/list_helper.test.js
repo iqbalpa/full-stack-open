54e9d44f22ff522a98fe5c888ae946bb7903e008
iqbalpa <iqbalpahlevi16@gmail.com>
2023-06-26 10:02:26 +0700

    part 4.5: done

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -24,3 +24,33 @@ describe("total likes", () => {
 		expect(result).toBe(5);
 	});
 });
+
+describe("favorite blog", () => {
+	const listOfBlog = [
+		{
+			_id: "5a422aa71b54a676234d17f8",
+			title: "Go To Statement Considered Harmful",
+			author: "Edsger W. Dijkstra",
+			url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+			likes: 5,
+			__v: 0,
+		},
+		{
+			_id: "5a422aa71b54a676234d17jk",
+			title: "Someone behind computer",
+			author: "Tom Holland",
+			url: "w3school.com",
+			likes: 10,
+			__v: 0,
+		},
+	];
+
+	test("return blog with most likes", () => {
+		const result = listHelper.favoriteBlog(listOfBlog);
+		expect(result).toEqual({
+			title: "Someone behind computer",
+			author: "Tom Holland",
+			likes: 10,
+		});
+	});
+});
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,7 +9,23 @@ const totalLikes = (blogs) => {
 	return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => {
+	let mostFavorite = blogs[0];
+	for (let blog of blogs) {
+		console.log(blog.title, blog.likes);
+		if (blog.likes >= mostFavorite.likes) {
+			mostFavorite = blog;
+		}
+	}
+	return {
+		title: mostFavorite.title,
+		author: mostFavorite.author,
+		likes: mostFavorite.likes,
+	};
+};
+
 module.exports = {
 	dummy,
 	totalLikes,
+	favoriteBlog,
 };
