9bbc406a8c072a51e246f04e66b27c87519c4baf
iqbalpa <iqbalpahlevi16@gmail.com>
2023-07-08 08:16:25 +0700

    part 5.7: done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,45 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+	const [showDetail, setShowDetail] = useState(false);
+
+	const hideWhenShowDetail = { display: showDetail ? "none" : "" };
+	const showWhenShowDetail = { display: showDetail ? "" : "none" };
+
+	const toggle = () => {
+		setShowDetail(!showDetail);
+	};
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	return (
+		<div>
+			<div style={hideWhenShowDetail}>
+				<div style={blogStyle}>
+					{blog.title} {blog.author}
+					<button onClick={toggle}>view</button>
+				</div>
+			</div>
+			<div style={showWhenShowDetail}>
+				<div style={blogStyle}>
+					<p>
+						{blog.title}
+						<button onClick={toggle}>hide</button>
+					</p>
+					<p>{blog.url}</p>
+					<p>
+						likes {blog.likes} <button>like</button>
+					</p>
+					<p>{blog.author}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Blog;
